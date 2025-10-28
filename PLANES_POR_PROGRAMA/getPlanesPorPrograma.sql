04d0b299e07ad8305f7e427eecf81abdc418a755
jhonry <jhonrydev@gmail.com>
2025-10-28 10:02:58 -0500

    Procedimiento para obtener los planes que están asociados a un Programa

--- PLANES_POR_PROGRAMA/getPlanesPorPrograma.sql
+++ PLANES_POR_PROGRAMA/getPlanesPorPrograma.sql
@@ -1,6 +1,3 @@
-USE [PreCoreMP]
-GO
-/****** Object:  StoredProcedure [saludmp].[SP_ADM_GET_INCONSISTENCIES_PROVIDERS]    Script Date: 9/01/2025 2:55:59 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -11,19 +8,33 @@ GO
   Fecha de creación: Enero 2025
   Objetivo: Permite obtener todos los planes que están relacionados con un programa
 */
-CREATE PROCEDURE [saludmp].[SP_ADM_GET_PLANES_POR_PROGRAMA]
+ALTER PROCEDURE [saludmp].[SP_ADM_GET_PLANES_POR_PROGRAMA_BORRAR]
     @coderror INTEGER  = 0 OUTPUT,
     @msgerror VARCHAR(500)  = 0 OUTPUT
 AS
 BEGIN
     BEGIN TRY
        
-        SELECT PG.plan_grupo cod_programa,PG.deno programa,P.plan_codi cod_plan,P.deno 'plan'
-        FROM PLANES P INNER JOIN PLANES_GRUPOS PG
-        ON P.plan_grupo = PG.plan_grupo
-        WHERE P.baja_fecha IS NULL
-        AND PG.baja_fecha IS NULL
-        ORDER by PG.plan_grupo;
+        -- SELECT TRIM(PG.plan_grupo) cod_programa,TRIM(PG.deno) programa,TRIM(P.plan_codi) cod_plan,TRIM(P.deno) 'plan'
+        -- FROM PLANES P INNER JOIN PLANES_GRUPOS PG
+        -- ON P.plan_grupo = PG.plan_grupo
+        -- WHERE P.baja_fecha IS NULL
+        -- AND PG.baja_fecha IS NULL
+        -- ORDER by PG.plan_grupo;
+
+        SELECT 
+        TRIM(P.plan_grupo) cod_programa, 
+        TRIM(P.deno) descripcion,
+        (
+            SELECT TRIM(PL.plan_codi )cod_plan, TRIM(PL.deno) descripcion
+            FROM PLANES PL
+            WHERE baja_fecha IS NULL AND PL.plan_grupo=P.plan_grupo
+            ORDER BY descripcion
+            FOR JSON PATH
+        ) planes
+        FROM PLANES_GRUPOS P
+        WHERE baja_fecha IS NULL
+        ORDER BY descripcion
 
         -- Establecer códigos de éxito y mensaje de éxito
         SET @coderror = 0;
@@ -40,3 +51,4 @@ BEGIN
         SET @msgerror = @ErrMsg;
     END CATCH
 END;
+GO
